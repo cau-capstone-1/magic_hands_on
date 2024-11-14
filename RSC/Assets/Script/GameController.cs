5beaceaad067d40ef82a6ce7449f29500da194c8
dmstjdhdh <71542970+dmstjdhdh@users.noreply.github.com>
2024-11-15 00:25:31 +0900

    Revert "config: FPS 속도 지정"
    This reverts commit 381835612eab1dbf2ec7b94eb12880d40a4880f6.

--- RSC/Assets/Script/GameController.cs
+++ RSC/Assets/Script/GameController.cs
@@ -5,9 +5,8 @@ public class GameController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Application.targetFrameRate = 60; // 60fps·Î Á¦ÇÑ
+        
     }
-
 
     // Update is called once per frame
     void Update()
