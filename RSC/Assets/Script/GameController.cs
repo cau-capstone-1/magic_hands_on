381835612eab1dbf2ec7b94eb12880d40a4880f6
dmstjdhdh <71542970+dmstjdhdh@users.noreply.github.com>
2024-11-15 00:24:51 +0900

    config: FPS 속도 지정

--- RSC/Assets/Script/GameController.cs
+++ RSC/Assets/Script/GameController.cs
@@ -5,8 +5,9 @@ public class GameController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Application.targetFrameRate = 60; // 60fps·Î Á¦ÇÑ
     }
+
 
     // Update is called once per frame
     void Update()
