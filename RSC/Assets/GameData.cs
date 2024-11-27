7e46b6af6e33d4b5bc3a928db6e7cd615a53fc94
hoondeveloper <hoonskyn9@gmail.com>
2024-11-28 01:36:32 +0900

    feat

--- RSC/Assets/GameData.cs
+++ RSC/Assets/GameData.cs
@@ -4,12 +4,35 @@ using UnityEngine;
 public class GameData : MonoBehaviour
 {
     public static GameData instance;
+
     public string? playerName {
         get {
             return PlayerPrefs.GetString("name");
         }
         set {
             PlayerPrefs.SetString("name", value);
+        }
+    }
+
+    public int playCount {
+        get {
+            if (PlayerPrefs.HasKey("playCount") == false) {
+                return 0;
+            }
+            
+            return PlayerPrefs.GetInt("playCount");
+        }
+        set {
+            PlayerPrefs.SetInt("playCount", value);
+        }
+    }
+
+    public string? startedAt {
+        get {
+            return PlayerPrefs.GetString("startedAt");
+        }
+        set {
+            PlayerPrefs.SetString("startedAt", value);
         }
     }
 
