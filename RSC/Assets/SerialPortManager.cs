4d1a536d5061946e417735b3acf5faac07e06eed
enk0de <32122626+enk0de@users.noreply.github.com>
2024-12-06 02:52:57 +0900

    feat: 포트 설정 기능 추가, UI 개선 등 (#16)
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * ㄹㄹ
    
    * fix
    
    * fix
    
    * fox
    
    * fox

---
+++ RSC/Assets/SerialPortManager.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SerialPortManager : MonoBehaviour
+{
+    public TMPro.TMP_Dropdown portDropdown; // 포트 선택 UI (Dropdown)
+    public TMPro.TMP_Text deviceInfoText; // 디바이스 정보 출력 UI
+
+    private string selectedPort = null;
+
+    void Start()
+    {
+        selectedPort = GameData.instance.serialPort;
+        List<string> ports = new List<string> { "" };
+
+        foreach (string port in SerialPort.GetPortNames())
+        {
+            ports.Add(port);
+        }
+
+        if (ports.Count == 0)
+        {
+            ports.Add("No Ports Available");
+        }
+
+        portDropdown.ClearOptions();
+        portDropdown.AddOptions(ports);
+    }
+
+    public void OnPortSelected(int index)
+    {
+        selectedPort = portDropdown.options[index].text;
+
+        GameData.instance.serialPort = selectedPort;
+
+        if (selectedPort != "No Ports Available")
+        {
+            StartCoroutine(ShowDeviceInfo(selectedPort)); // 3초 타임아웃
+        }
+        else
+        {
+            deviceInfoText.text = "No device connected.";
+        }
+    }
+
+    private IEnumerator ShowDeviceInfo(string portName)
+    {
+        deviceInfoText.text = "Connecting...";
+        string response = "Timeout: No response from device.";
+
+        Thread thread = new Thread(() =>
+        {
+            try
+            {
+                using (SerialPort serialPort = new SerialPort(portName, 9600))
+                {
+                    serialPort.Open();
+                    response = serialPort.PortName;
+                }
+            }
+            catch (TimeoutException)
+            {
+                response = "Timeout: No response from device.";
+            }
+            catch (System.Exception ex)
+            {
+                response = $"Error: {ex.Message}";
+            }
+        });
+
+        thread.Start();
+        while (thread.IsAlive)
+        {
+            yield return null; // 스레드가 완료될 때까지 대기
+        }
+
+        deviceInfoText.text = response;
+    }
+}
